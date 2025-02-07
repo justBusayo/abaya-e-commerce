62a30631a28c6c14dd319c94299cfeeac874a941
mhcServices <miracle2homecare@gmail.com>
2025-02-07 13:37:42 +0100

    passport cope for google auth

--- backend/controllers/socialauth.controller.js
+++ backend/controllers/socialauth.controller.js
@@ -53,6 +53,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      passReqToCallback: true,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
